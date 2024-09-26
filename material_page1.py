84d62f947cb0e7b6292fe500ac3615b0f5d262d0
mjb-v <square3325@gmail.com>
2024-09-26 09:55:54 +0900

    0926 update

--- material_page1.py
+++ material_page1.py
@@ -6,13 +6,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import requests
 from datetime import datetime
+import os
+from dotenv import load_dotenv
 
 font_path = 'NanumGothic-Regular.ttf'
 font_manager.fontManager.addfont(font_path)
 rc('font', family='NanumGothic')
 
 # FastAPI URL
-API_URL = "http://127.0.0.1:8000"
+load_dotenv()
+API_URL = os.getenv("API_URL")
 
 # 한글 컬럼명으로 변환
 def translate_data(data):
@@ -36,6 +39,10 @@ def translate_data(data):
     }
     return pd.DataFrame(data).rename(columns=translation_dict)
 
+# 1-1. 위 - 전체 플랜
+
+
+# 1-2. 아래 - 당월 플랜
 def get_material_all_plan(year: int, month: int):
     url = f"{API_URL}/materials/rate/{year},{month}"
     response = requests.get(url)
@@ -44,48 +51,54 @@ def get_material_all_plan(year: int, month: int):
     else:
         st.error("데이터를 불러오는 데 실패했습니다.")
         return None
+
+# 2. 등록 페이지 GET
+
+
+
+
 # ----------------------------------------------------------------
 def material_page1_view():
     pass
 
-st.title("자재 계획 관리")
-tab = st.sidebar.selectbox("", ["자재 계획 관리", "자재 계획 등록"])
+# st.title("자재 계획 관리")
+# tab = st.sidebar.selectbox("", ["자재 계획 관리", "자재 계획 등록"])
 
-if tab == "자재 계획 관리":
-    st.subheader("자재 계획")
-    selected_year = st.selectbox("년도 선택", list(range(2014, 2025)))
-    material_plan_data = get_material_all_plan(selected_year)
+# if tab == "자재 계획 관리":
+#     st.subheader("자재 계획")
+#     selected_year = st.selectbox("년도 선택", list(range(2014, 2025)))
+#     material_plan_data = get_material_all_plan(selected_year)
 
-    if material_plan_data:
-        df = pd.DataFrame(material_plan_data)
-        df = translate_data(df)
-        df = df.drop(columns=["년도"])
+#     if material_plan_data:
+#         df = pd.DataFrame(material_plan_data)
+#         df = translate_data(df)
+#         df = df.drop(columns=["년도"])
 
-        # 테이블 형식으로 변환 (month를 columns로, 나머지를 index로 변환)
-        df_pivot = df.set_index('월').T
-        df_pivot.columns = [f"{month}월" for month in df_pivot.columns]  # month를 '월'로 변환
+#         # 테이블 형식으로 변환 (month를 columns로, 나머지를 index로 변환)
+#         df_pivot = df.set_index('월').T
+#         df_pivot.columns = [f"{month}월" for month in df_pivot.columns]  # month를 '월'로 변환
 
-        # 테이블 출력
-        st.subheader(f"{selected_year}년도 계획 및 실적 데이터")
-        st.dataframe(df_pivot)
+#         # 테이블 출력
+#         st.subheader(f"{selected_year}년도 계획 및 실적 데이터")
+#         st.dataframe(df_pivot)
 
-        # 그래프
-        st.subheader(f"{selected_year}년 차트")
+#         # 그래프
+#         st.subheader(f"{selected_year}년 차트")
 
-        # 월별로 사업 달성률과 생산 달성률 데이터 추출
-        business_achievement_rates = df["사업달성율"]
-        months = df["월"].apply(lambda x: f"{x}월")
+#         # 월별로 사업 달성률과 생산 달성률 데이터 추출
+#         business_achievement_rates = df["사업달성율"]
+#         months = df["월"].apply(lambda x: f"{x}월")
 
-        fig, ax = plt.subplots(figsize=(8, 6))
+#         fig, ax = plt.subplots(figsize=(8, 6))
 
-        # 막대그래프에 월별 데이터 추가
-        ax.bar(months, business_achievement_rates, width=0.4, label='사업 달성률', align='center', color='#ff9999')
+#         # 막대그래프에 월별 데이터 추가
+#         ax.bar(months, business_achievement_rates, width=0.4, label='사업 달성률', align='center', color='#ff9999')
 
-        # 그래프에 텍스트와 제목 추가
-        ax.set_ylim(0, 100)  # y축 범위 0~100
-        ax.set_ylabel('달성률 (%)')
-        ax.set_title(f"{selected_year}년 월별 사업 달성률")
-        ax.legend()
-        st.pyplot(fig)
-    else:
-        st.warning("계획 데이터가 없습니다.")
+#         # 그래프에 텍스트와 제목 추가
+#         ax.set_ylim(0, 100)  # y축 범위 0~100
+#         ax.set_ylabel('달성률 (%)')
+#         ax.set_title(f"{selected_year}년 월별 사업 달성률")
+#         ax.legend()
+#         st.pyplot(fig)
+#     else:
+#         st.warning("계획 데이터가 없습니다.")
